e8b17716bcfe8332b6e8e890aa44ef0b980ef039
Mario Capitbrok <mario@capitbrok.com>
2022-08-02 14:30:32 -0500

    13- Project- Restructure the App

--- .history/index_20220802142657.js
+++ .history/index_20220802142657.js
@@ -4,7 +4,14 @@ const genres = require('./routes/genres')
 const app = express()
 
 app.use(express.json())
+
 app.use('/api/genres', genres)
+
+let genres = [
+  { id: 1, name: 'Action' },
+  { id: 2, name: 'Comedy' },
+  { id: 3, name: 'Thriller' },
+]
 
 app.get('/', (req, res) => {
   res.send('Hello Mario!!!')
--- .history/index_20220802142737.js
+++ .history/index_20220802142737.js
@@ -4,6 +4,7 @@ const genres = require('./routes/genres')
 const app = express()
 
 app.use(express.json())
+
 app.use('/api/genres', genres)
 
 app.get('/', (req, res) => {
